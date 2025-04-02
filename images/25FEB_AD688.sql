f86f2e131de9ca2b2b682d71f574082fe903d4f7
Pranjul Garg <86375765+Prawnz11@users.noreply.github.com>
2025-04-01 20:24:58 -0400

    changing the theme to quartz

---
+++ images/25FEB_AD688.sql
@@ -0,0 +1,6 @@
+CREATE TABLE AD688_A1 (id INT,
+                       first_name VARCHAR(255),
+                       last_name VARCHAR(255),
+                      course_name VARCHAR(255),
+                       country VARCHAR(255));
+                       
