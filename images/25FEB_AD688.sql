56aa574842d4e584df38da4ef8e9b70a221ac97f
Pranjul Garg <86375765+Prawnz11@users.noreply.github.com>
2025-04-01 20:36:00 -0400

    Cleared the unwanted files and tabs to start afresh

--- images/25FEB_AD688.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE AD688_A1 (id INT,
-                       first_name VARCHAR(255),
-                       last_name VARCHAR(255),
-                      course_name VARCHAR(255),
-                       country VARCHAR(255));
-                       
